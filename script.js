c2674c30acb0f94f2af1ce1fe5f5bfda0ec3e02f
Nikita Stepanovs(nixory) <nikitastepanovs777@gmail.com>
2022-05-16 21:44:15 +0300

    17 lessob

--- script.js
+++ script.js
@@ -148,3 +148,43 @@ function learnJS(lang, callback) {
 learnJS('JavaScript', function(){
     console.log('YEAASDSA');
 });
+
+
+const  options = {
+    name: 'test',
+    with: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+
+    makeTest: function() {
+        console.log("Test")
+    }
+};
+
+options.makeTest();
+
+const{border, bg} = options.colors;
+console.log(border);
+//console.log(Object.keys(options).length);
+
+
+
+
+let counter = 0;
+for (let key in options) {
+    if ( typeof(options[key]) === 'object'){
+        for ( let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+    
+}
+
+console.log(counter);
