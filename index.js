bf3c68af19d588dc61ab92f18bc1b4f9799b3c8e
nixory <nikitastepanovs777@gmail.com>
2023-12-13 17:18:23 +0200

    lesson 2 finished all tasks

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
-let a = 7;
-let b = 9;
+// let a = 7;
+// let b = 9;
+//
+// console.log(a * b)
+// let c = 7;
+// let d = 9;
+// console.log(c /d );
+// let e = 3;
+// let f = 5;
+// console.log(e + f  );
+//
+// let e1 = '3';
+// let f1 = 5 ;
+// console.log(e1 + f1);
+//
+// let e2 = 3; let f2 = 0;
+// console.log(e2 / f2)
+//
+// let e3 = 3; let f3 = 'Hello';
+// console.log(e3 + f3)
+//
+// let e4 = 3; let f4 = 'Hello';
+// console.log(e2 * f2)
+//
+//
+//
+// let name = document.querySelector('.name');
+// let surname = document.querySelector('.surname');
+// let one = document.querySelector('.one');
+// let two = document.querySelector('.two');
+// let btn = document.querySelector("button");
+// let output = document.querySelector("h2");
+// // btn.onclick = function () {
+// //     let nameValue = name.value;
+// //     let surnameValue = surname.value;
+// //     output.innerHTML = 'Hello' + ' ' + nameValue + ' ' +  surnameValue;
+// //     name.value = surname.value = "";
+// // }
+//
+// // btn.onclick = function () {
+// //     let nameValue = name.value;
+// //     let surnameValue = +surname.value;
+// //
+// //     console.log(+nameValue)
+// //     name.value = surname.value = "";
+// // }
+//
+// let y = document.querySelector('.sevenseven');
+// y.style.border = '2px solid red';
+//
+// btn.onclick = function () {
+//     let t = one.value;
+//     t = parseInt(t)
+//     console.log(t)
+//     t.value = "";
+// }
 
-console.log(a * b)
+
+let btn = document.querySelector("button");
+
+btn.onclick = function () {
+    let userName = document.querySelector('.name').value;
+    let userSurname = document.querySelector('.surname').value;
+    let userAge = document.querySelector('.age').value;
+    let userHobby = document.querySelector('.hobby').value;
+    document.querySelector("h2").innerHTML = 'Уважаемый ' + userName + ', ' + userSurname
+        + ', ваш возраст ' + userAge + ' года, ваше хобби: ' + userHobby;
+}
+
